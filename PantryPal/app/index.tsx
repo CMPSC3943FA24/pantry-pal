df7b6946ff1cc3424196ac996be571c7918749fd
Luke-Turner1977 <luke.c.turner77@gmail.com>
2024-09-29 16:20:42 -0500

    Eliminated a typo

--- PantryPal/app/index.tsx
+++ PantryPal/app/index.tsx
@@ -4,8 +4,17 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to PantryPal</Text>
-      <Text style={styles.subtext}>You can begin working on the app in /app/index.tsx</Text>
+      <Text style={styles.text}>What would you like to filter?</Text>
+      <Text style={styles.subtext}>Expiring soon</Text>
+      <Text style={styles.subtext}>Running Low</Text>
+      <Text style={styles.subtext}>Dietary Preferences</Text>
+      <Text style={styles.subtext}>Grocery Category</Text>
+      <Text style={styles.subtext}>Breakfast</Text>
+      <Text style={styles.subtext}>Lunch</Text>
+      <Text style={styles.subtext}>Supper</Text>
+      <Text style={styles.subtext}>Dessert</Text>
+      <Text style={styles.subtext}>Snacks</Text>
+      <Text style={styles.subtext}>Location</Text>
     </View>
   );
 }
